916ac0349b104e4932f89ef2c2fc06fe984d8c76
Hitesh Bhamre <108949778+hiteshbhamre07@users.noreply.github.com>
2025-07-30 19:45:13 +0530

    Update urls.py

--- myapi_project/api/urls.py
+++ myapi_project/api/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('students/', views.student_list, name='student-list'),
     path('students/create/', views.student_create, name='student-create'),
+    path('students/<int:pk>/', views.student_detail, name='student-detail'),
+    path('students/<int:pk>/update/', views.student_update, name='student-update'),
+    path('students/<int:pk>/delete/', views.student_delete, name='student-delete'),
 ]
